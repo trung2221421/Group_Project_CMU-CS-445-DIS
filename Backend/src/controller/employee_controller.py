91a24e4936fb39ec04f4170c4ca2c886cbbe2f21
thevu1 <temnguyenvu@gmail.com>
2026-05-01 21:26:44 +0700

    newBackEnd

--- Backend/src/controller/employee_controller.py
+++
@@ -1,9 +0,0 @@
-# src/controllers/employee_controller.py
-from fastapi import APIRouter
-from src.services.employee_service import get_employee_list
-
-router = APIRouter()
-
-@router.get("/employees")
-def get_employees():
-    return get_employee_list()
